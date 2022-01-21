fe4bb9d85993fef46c59013be47df5b3d9215e6d
Jose Jimenez <email@jimenezjose.com>
2022-01-21 13:14:23 -0600

    init commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Marketplace</title>
+        <meta name="description" content="Marketplace" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Marketplace!</h1>
+      </main>
+    </div>
+  );
+}
