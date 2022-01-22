cabdcc025a4d888754d5f1e6ec561b13a1ab7bbf
Jose Jimenez <email@jimenezjose.com>
2022-01-22 01:06:40 -0600

    add home functionality

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,25 @@
 import * as React from "react";
 import SearchBox from "./SearchBox";
-import { HStack } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import logo from "../public/logo.png";
 
 export default function Navbar() {
+  const { push } = useRouter();
   return (
     <HStack
-      sx={{ padding: "20px", justifyContent: "center" }}
+      sx={{ padding: "20px", justifyContent: "space-between" }}
       backgroundColor="orange.200"
     >
+      <Box
+        sx={{ cursor: "pointer", marginTop: "6px" }}
+        onClick={() => push("/")}
+      >
+        <Image src={logo} alt="logo" width={70} height={32} />
+      </Box>
       <SearchBox />
+      <Box />
     </HStack>
   );
 }
